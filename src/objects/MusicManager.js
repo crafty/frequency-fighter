7ef6c05f292d13db0ed766ac91229b31d00930df
Dan <craycraftdan@gmail.com>
2019-10-16 21:49:42 -0400

    Worked out a better way to handle the music manager

--- src/objects/MusicManager.js
+++ src/objects/MusicManager.js
@@ -1,12 +1,3 @@
-/*
-const Tone = require("Tone");
-const toneAnalyzer = new Tone.Analyser({
-  size: 1024,
-  type: "fft",
-  smoothing: 0.8
-});
-*/
-
 class MusicManager {
   constructor(scene) {
     //super(scene);
@@ -37,74 +28,33 @@ class MusicManager {
     this.analyzerNode.source = scene.sound.sounds[0].source;
     this.analyzerNode.source.connect(this.analyzerNode);
 
-    /* The below 3 items are to help the changes in data be less dramatic, smoothingTimeConstant overlaps values in the data arrays */
-    this.analyzerNode.minDecibels = -90;
-    this.analyzerNode.maxDecibels = -10;
-    this.analyzerNode.smoothingTimeConstant = 0.8;
+    /* Lots of Smoothing, The below 3 items are to help the changes in data be less dramatic, smoothingTimeConstant overlaps values in the data arrays */
+    this.analyzerNode.smoothingTimeConstant = 0.5;
+    this.analyzerNode.fftSize = 1024; // 512 2048(default)
 
-    this.bufferLength = this.analyzerNode.frequencyBinCount;
+    this.bufferLength = this.analyzerNode.frequencyBinCount; // frequencyBinCount = half the fftSize
 
     this.freqTimeData = new Uint8Array(this.bufferLength);
     this.freqByteData = new Uint8Array(this.bufferLength);
+  }
 
-    //toneAnalyzer.connect(this.analyzerNode.context);
-
-    /*
-      Audio Tempo Processor
-    */
-    // this.onAudioProcess = new RealTimeBPMAnalyzer({
-    //   scriptNode: {
-    //     bufferSize: 4096,
-    //     numberOfInputChannels: 1,
-    //     numberOfOutputChannels: 1
-    //   },
-    //   pushTime: 2000,
-    //   pushCallback: (err, bpm) => {
-    //     // if (err) console.log(err);
-    //     // console.log("bpm", bpm);
-    //   }
-    // });
-
-    // const scriptProcessorNode = scene.sound.context.createScriptProcessor(
-    //   4096,
-    //   1,
-    //   1
-    // );
-
-    // scriptProcessorNode.connect(destination);
-    // scene.sound.sounds[0].source.connect(scriptProcessorNode);
-
-    // scriptProcessorNode.onaudioprocess = e => {
-    //   this.onAudioProcess.analyze(e);
-    // };
-
-    // /* Delay Time in ms for quarter-note beats */
-    // const msPerMinute = 60000; // 60,000 ms (1 minute)
-    // const zeroToOneHundredBPM = 96; // Tempo (BPM)
-    // const delayTime = msPerMinute / zeroToOneHundredBPM; // Tempo-Sync Echo Delay Time
-
-    /* 
-      Tone Transport | Time Keeping 
-    */
-    // Transport.scheduleRepeat(function(time) {
-    //   //do something with the time                  HETRERE TONE TRANSPORT LEFT OFF
-    //   console.log("test");
-    //   //this.scene.sound.play("tock", { volume: 1.5 });
-    // }, "8n");
-
-    /* 
-      Beat Detector 
-    */
-    // analyze(this.analyzerNode.audioBuffer)
-    //   .then(tempo => {
-    //     console.log("BEAT", tempo);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
+  byteToNormalizedFloat(srcArray, dstArray) {
+    /* Converts bytes to normalized (0 <-> 1) floats */
+    const sum = srcArray.reduce((pV, cV) => (pV += cV / 255));
+    srcArray.forEach((point, index) => {
+      dstArray[index] = sum / srcArray.length;
+    });
   }
 
   update() {
+    /* ============ NOTES ============ */
+    /* Setting opacity based on feqByteData */
+    /* Byte values range between 0 - 255, that maps to -1 <=> +1, s0 128 = 0 */
+    /* getFloatFrequencyData returns dB, getByteFrequencyData maps based on min/max decibels */
+    /* Must use Float32Array instead of Unit8Array for Float arrays */
+    /* Amplitude = sum of freqByteData / (this.bufferLength * 256 - 1); */
+    /* ============ NOTES ============ */
+
     /* Update Current Time Of Song */
     this.time = this.analyzerNode.context.currentTime;
 
@@ -112,24 +62,29 @@ class MusicManager {
     //this.analyzerNode.getByteTimeDomainData(this.freqTimeData);
     this.analyzerNode.getByteFrequencyData(this.freqByteData);
 
-    /* Setting opacity based on feqByteData */
-    let average = Math.round(
-      this.freqByteData.reduce((pV, cV) => (pV += cV)) /
-        this.freqByteData.length
-    );
+    /* Create a float 32 array */
+    this.floatArray = new Float32Array(this.bufferLength);
 
-    let percent = average / 100;
+    /* Convert the byte data to normalized(0 - 1 values) Float32Array */
+    this.byteToNormalizedFloat(this.freqByteData, this.floatArray);
 
-    if (percent > 0.85) {
-      this.scene.stars.starfieldFront.alpha = 1;
-      this.scene.stars.starfieldMid.alpha = 1;
-    } else if (percent < 0.15) {
-      this.scene.stars.starfieldFront.alpha = 0.2;
-      this.scene.stars.starfieldMid.alpha = 0.2;
-    } else {
-      console.log(percent);
-      this.scene.stars.starfieldFront.alpha = percent;
-      this.scene.stars.starfieldMid.alpha = percent;
+    /* Filters 0's out of the float array */
+    this.floatArray = this.floatArray.filter(value => value !== 0);
+
+    /* Get the average of the normalized data */
+    /* If the foatArray has values */
+    if (this.floatArray.length > 0) {
+      let average =
+        this.floatArray.reduce((pV, cV) => (pV += cV)) / this.floatArray.length;
+
+      /* Add dd to the average if you want a higher normalized value */
+      average += 0.3;
+
+      console.log(average);
+
+      /* Use the average to set opacity */
+      this.scene.stars.starfieldFront.alpha = average;
+      this.scene.stars.starfieldMid.alpha = average;
     }
   }
 }
